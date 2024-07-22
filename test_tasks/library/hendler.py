1262018a92d0132704c22244fe3be4875836d5fb
Pavel <room.job@mail.ru>
2024-07-22 15:46:17 +0300

    Added create, update, read

--- test_tasks/library/hendler.py
+++ test_tasks/library/hendler.py
@@ -1,29 +1,69 @@
+import json
+import time
+
+
 class Library:
     def __init__(self) -> None:
-        self.id = "id"
+        self.id = int(time.time() * 1000)
+        self.data = "data.json"
         self.title = "title"
         self.author = "author"
         self.year = "year"
         self.status = "status"
 
-    def create(self, lst):
-        lst.append(
-            {
-                self.id: len(lst) + 1,
-                self.title: input("Введите название книги: "),
-                self.author: input("Введите автора: "),
-                self.year: int(input("Введите год: ")),
-                self.status: "В наличии",
-            },
-        )
-        print(lst)
-        return lst
+    def create(self):
+        with open("data.json", "w", encoding="utf-8") as file:
+
+            dct = {
+                input("Введите автора: "): {
+                    self.id: {
+                        self.title: input("Введите название книги: "),
+                        self.year: int(input("Введите год: ")),
+                        self.status: "В наличии",
+                    },
+                },
+            }
+            return json.dump(dct, file, ensure_ascii=False, indent=2)
+
+    def update(self):
+        with open(self.data, encoding="utf-8") as file:
+            self.id = int(time.time() * 1000)
+            data = json.load(file)
+            self.author = input("Введите автора: ")
+            if self.author not in data:
+                data.update(
+                    {
+                        self.author: {
+                            self.id: {
+                                self.title: input("Введите название книги: "),
+                                self.year: int(input("Введите год: ")),
+                                self.status: "В наличии",
+                            },
+                        },
+                    },
+                )
+            else:
+                data[self.author][self.id] = {
+                    self.title: input("Введите название книги: "),
+                    self.year: int(input("Введите год: ")),
+                    self.status: "В наличии",
+                }
+
+        with open(self.data, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
 
     def delete(self):
+        # with open(self.data, encoding="utf-8") as file:
+        #     data = json.load(file)
+        #     remove_id = int(input())
+        #     for
         pass
 
     def read(self):
-        pass
+        with open(self.data, encoding="utf=8") as file:
+            data = json.load(file)
+            for key, value in data.items():
+                print(f"\nid={key}: {value}")
 
     def change_status(self):
         pass
--- test_tasks/library/main.py
+++ test_tasks/library/main.py
@@ -1,7 +1,7 @@
-from rich import print
+import os
 
-from test_tasks.library.hendler import Library
-from test_tasks.library.test_info import books
+from hendler import Library
+from rich import print
 
 library = Library()
 
@@ -9,32 +9,38 @@ library = Library()
 def main():
     print(
         """
-        1. Добавить книгу,
-        2. Удалить книгу,
-        3. Отобразить все книги,
-        4. Изменить статус книги
-        5. Выход
-        """
+            1. Добавить книгу,
+            2. Удалить книгу,
+            3. Отобразить все книги,
+            4. Изменить статус книги
+            5. Выход
+            """
     )
-    try:
-        choice = int(input("Введите число для выбора (1-5) "))
-        match choice:
-            case 1:
-                library.create(books)
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
-            case 5:
-                exit()
-            case _:
-                print("Введите число от 1 до 4 или проваливайте")
-                main()
-    except ValueError:
-        print("Дурак? Сказали же число ввести")
-        main()
+    # try:
+
+    match int(input("Введите число для выбора (1-5) ")):
+        case 1:
+            (
+                library.update()
+                if os.path.exists("data.json")
+                else library.create()
+            )
+        case 2:
+            pass
+        case 3:
+            library.read()
+        case 4:
+            pass
+        case 5:
+            exit()
+        case _:
+            print("Введите число от 1 до 5 или проваливайте")
+            main()
+    main() if input("Выполнить еще? ") == "y" else exit()
+
+    # except ValueError:
+    #     print("Дурак? Сказали же число ввести")
+    #     main()
 
 
 if __name__ == "__main__":
